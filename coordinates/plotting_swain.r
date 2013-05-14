297814f0790e3bf18830e18214260b848646bf53
ivihernandez <ivihernandez@gmail.com>
2013-05-14 14:11:40 -0400

    2nd commit

--- coordinates/plotting_swain.r
+++ coordinates/plotting_swain.r
@@ -1,8 +1,10 @@
 dataset <- swain
-plot <- ggplot(data=dataset, aes(x=x,y=y,size=weight)) + geom_point() + theme_bw(18) 
-plot <- plot + xlab('X Coordinate') + ylab('Y Coordinate')
-plot <- plot + theme(axis.title.y = element_text( face='bold'))
-plot <- plot + theme(axis.title.x = element_text( face='bold'))
-plot <- plot + labs(size='Weight')
-plot <- plot + coord_fixed()
+
+library(ggplot2)
+myplot <- ggplot(data=dataset, aes(x=x,y=y,size=weight)) + geom_point() + theme_bw(18) 
+myplot <- myplot + xlab('X Coordinate') + ylab('Y Coordinate')
+myplot <- myplot + theme(axis.title.y = element_text( face='bold'))
+myplot <- myplot + theme(axis.title.x = element_text( face='bold'))
+myplot <- myplot + labs(size='Weight')
+myplot <- myplot + coord_fixed()
 plot
